26fc065c69a37651140bff8dc6a261d26110e7d4
sabir kral <sabirmemmedli21152014@gmail.com>
2026-02-25 21:02:03 +0400

    sssssssss

--- Main.java
+++ Main.java
@@ -2,17 +2,41 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4,4};
-        System.out.println(hasDuplicate(arr));
-    }
 
-    public static boolean hasDuplicate(int[] arr) {
-        Arrays.sort(arr);
-        for (int i = 1; i < arr.length; i++) {
-            if (arr[i] == arr[i - 1]) {
-                return true;
+        int[] arr1 = {1, 2,3,7};
+
+        int length = arr1.length;
+
+
+        int[] arr2 = new int[length];
+
+
+        for (int i = 0; i < length; i++) {
+            arr2[i] = i;
+        }
+
+
+        for (int i = 0; i < length; i++) {
+
+        }
+
+
+
+        for (int i = 0; i < arr1.length; i++) {
+            boolean tapildi = false;
+
+            for (int j = 0; j < arr2.length; j++) {
+                if (arr1[i] == arr2[j]) {
+                    tapildi = true;
+                    break;
+                }
+            }
+
+            if (!tapildi) {
+                System.out.println(arr1[i]);
             }
         }
-        return false;
     }
+
+
 }
