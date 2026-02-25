185741686b258a47735a74b24f86a0a6b11058af
sabir kral <sabirmemmedli21152014@gmail.com>
2026-02-25 20:25:07 +0400

    ss

--- Main.java
+++ Main.java
@@ -1,47 +1,23 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        //1
-        Integer[] list = {1,3,-3,0,-3,2,-5,4,0,-3,-2};
-        for (Integer b:list){
-            if (b<0){
-                System.out.println(b);
+        Integer[] i = {1,1,34,2,6,23,5,3};
+        Integer target = 5;
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int j = 0; j < i.length; j++) {
+            int complement = target - i[j];
+
+            if (map.containsKey(complement)) {
+                System.out.println("Index: " + map.get(complement) + " and " + j);
+                return;
             }
-        }
-        System.out.println("---------------------------------");
-        //2
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("bir reqem yaz");
-        Integer number = scanner.nextInt();
 
-        int fact = 1;
-
-        for (int i = 1; i <= number; i++) {
-            fact *= i;
+            map.put(i[j], j);
         }
 
-        System.out.println("factorial:"+number+"="+fact);
-        //3
-        Integer[] array = {1,3,3,0,3,2,5,4,0,3,2};
-        for (Integer b:array){
-            if (b%2==1){
-                System.out.println(b+b);
-            }
-        }
-        System.out.println("---------------------------------------");
-        //4
-       Scanner sc = new Scanner(System.in);
-        System.out.println("bir reqem yaz");
-        Integer num = sc.nextInt();
-        if(num%2==0&&num%num==0){
-            System.out.println("sade deyil");
-        } else {
-            System.out.println("sadedir");
-        }
-        //5?
+        System.out.println("Not Found");
     }
-}
+    }
