e51adba04c6842f7e7e30ffebf624022366225ca
sabir kral <sabirmemmedli21152014@gmail.com>
2026-02-28 17:37:27 +0400

    ssssssssss

--- Main.java
+++ Main.java
@@ -2,30 +2,42 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        //1 ?
+        //2
 
-        int[] arr = {0, 5, 0, 3, 8, 0, 1};
+        int[] arr = {0, 5, 3, 8,1};
+        int[] arr2 = {3, 8, 0, 1};
+        Set<Integer> arr3 = new HashSet<>();
 
-        moveZerosToEnd(arr);
-        System.out.println(Arrays.toString(arr));
-    }
-
-    public static int[] moveZerosToEnd(int[] arr) {
-        int index = 0;
+        for (int a :arr ){
+            for (int b:arr2){
+                if (b==a){
+                        arr3.add(b);
 
 
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] != 0) {
-                arr[index] = arr[i];
-                index++;
+                }
+
             }
         }
+        System.out.println(arr3);
+            //3
+                Integer[] massiv = {1,5,2,3};
+                Integer[] massiv2 = {5,4,7,6};
+                Set<Integer> massiv3 = new HashSet<>();
+                Arrays.sort(massiv);
+                Arrays.sort(massiv2);
+                for (Integer a:massiv){
+                    for (Integer b:massiv2){
+                        massiv3.add(a);
+                        massiv3.add(b);
+                    }
+                }
+
+        System.out.println(massiv3);
+        //4 ?
+        //5 ?
 
 
-        while (index < arr.length) {
-            arr[index] = 0;
-            index++;
-        }
-
-return new int[]{} ;
     }
+
 }
