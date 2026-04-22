4ed67ba4c067a5f7b75edf733501e6c166b62c54
sabir kral <sabirmemmedli21152014@gmail.com>
2026-04-22 21:23:40 +0400

    s

---
+++ Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        //1
+        int[] arr = {2,34,3,3,3,3,6,23,345,456,345,23,67,3,45,32,35};
+        for (int a:arr){
+            if (a%2==0&&a%3==0){
+                System.out.println(a);
+            }
+        }
+        //2 ?
+        //3 ?
+        //4 ?
+        //5 6 7 ?
+
+    }
+}
